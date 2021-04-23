e9f58cece1c0d693475d82de2e49f911bd68fd15
Gabriel Bittencourt <79853847+gabrlcj@users.noreply.github.com>
2021-04-23 13:56:48 -0300

    Código do Conversor de moedas

---
+++ conversor-de-moedas/README.markdown
@@ -0,0 +1,5 @@
+# Conversor de Moedas
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/gabrlcj/pen/QWdWGXR](https://codepen.io/gabrlcj/pen/QWdWGXR).
+
+
---
+++ conversor-de-moedas/src/script.js
@@ -0,0 +1,16 @@
+    let valorInicial = document.getElementById("valor-inicial");
+    let tipoDaMoeda = document.getElementById("tipo-da-moeda");
+    let valorResultante = document.getElementById("res");
+ 
+    const moeda = () => {
+        let valorDaMoeda = 0
+        if(tipoDaMoeda.value === "Dólar 🇺🇸") {
+            valorDaMoeda = 5.54
+        }else if(tipoDaMoeda.value === "Euro 🇪🇺") {
+            valorDaMoeda = 6.60
+        }else {
+            valorDaMoeda = 7.66
+        }
+        let valorConvertido = (valorInicial.value * valorDaMoeda).toFixed(2)
+        return valorResultante.innerHTML = "R$" + valorConvertido
+    }
